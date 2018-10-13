9d1a3ee952e6375b8e1da38208855dc410ddeb4e
Jason Nelson <jason@carbonmade.com>
2018-10-13 15:59:58 -0700

    Format PdfBookmark

--- PdfBookmarkCollection.cs
+++ PdfBookmarkCollection.cs
@@ -1,25 +1,21 @@
 ﻿using System.Collections.ObjectModel;
 
-#pragma warning disable 1591
-
 namespace PdfiumLight
 {
     public class PdfBookmark
     {
-        public string Title { get; set; }
-        public int PageIndex { get; set; }
-
-        public PdfBookmarkCollection Children { get; set; }
-
         public PdfBookmark()
         {
             Children = new PdfBookmarkCollection();
         }
 
-        public override string ToString()
-        {
-            return Title;
-        }
+        public string Title { get; set; }
+
+        public int PageIndex { get; set; }
+
+        public PdfBookmarkCollection Children { get; set; }
+
+        public override string ToString() => Title;
     }
 
     public class PdfBookmarkCollection : Collection<PdfBookmark>
